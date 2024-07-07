cb875586d34af36b83d49b2a5064c0606e165955
Ossama Samir <147268769+EngOssamaSameer@users.noreply.github.com>
2024-07-08 01:09:09 +0300

    add

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetWebAPIAuth.Data
+{
+    public class AppDbContext : IdentityDbContext 
+    {
+        public AppDbContext(DbContextOptions opt) : base(opt)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+using DotNetWebAPIAuth.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    opt.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+
+
+//add conection to database
+builder.Services.AddDbContext<AppDbContext>(opt=>{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("myConnection"));
+});
+//add Authorizatio
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+//add identity end point
+builder.Services.AddIdentityApiEndpoints<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapIdentityApi<IdentityUser>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
